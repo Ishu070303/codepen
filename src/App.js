fb24febc092cf1398a5e5288c85c20a1f37c36bd
Eshika rawat <eshikarawat02@gmail.com>
2023-11-21 17:04:16 +0530

    added a react project,routes,tailwindcss

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Navigate, Route, Routes } from 'react-router-dom';
+
+import { Home } from './containers';
+
+const App = () => {
+  return (
+    <div className='w-screen h-screen flex items-start justify-start overflow-hidden'>
+        <Routes>
+            <Route path='/home/*' element={<Home />} />
+
+            {/* If URL did not match */}
+            <Route path='*' element={<Navigate to={"/home"} />} />
+        </Routes>
+    </div>
+  )
+}
+
+export default App;
