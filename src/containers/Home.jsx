6b8756013118405ea2fe52f8ffd15dcab62c46df
Eshika rawat <eshikarawat02@gmail.com>
2023-12-20 21:26:23 +0530

    added icon to the dashboard

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,8 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { HiChevronDoubleLeft } from 'react-icons/hi2';
 
 const Home = () => {
+  
+  const [ isSideMenu, setIsSideMenu ]  = useState(false);
+
   return (
-    <div>Home</div>
+    <>
+    <div 
+    className={`w-2 
+    ${
+      isSideMenu ? "w-2" : "flex-[.2] xl:flex-[.2]"
+    } 
+    min-h-screen max-h-screen relative bg-black`}
+    >
+      {/* anchor */}
+      <div className='w-8 h-8 bg-secondary rounded-tr-lg rounded-br-lg
+      absolute -right-6 flex items-center justify-center cursor-pointer'>
+        <HiChevronDoubleLeft className='text-white text-xl' />
+      </div>
+      {/* logo */}
+
+      {/* start coding */}
+
+      {/* home nav */}
+    </div>
+    <div></div>
+    </>
   )
 }
 
