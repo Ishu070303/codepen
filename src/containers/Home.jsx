d7beb706d529280f13faf0ab0196a08cf3c2bc4a
Eshika rawat <eshikarawat02@gmail.com>
2023-12-21 20:14:38 +0530

    formatted home page

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,116 +1,118 @@
-import React, { useState } from 'react';
-import { HiChevronDoubleLeft } from 'react-icons/hi2';
-import { MdHome } from 'react-icons/md';
-import { FaSearchengin } from 'react-icons/fa';
-import { motion  } from 'framer-motion';
-import { Link, Route, Routes } from 'react-router-dom';
-import { Logo } from '../assets';
-import { Projects, SignUp } from '../containers';
+import React, { useState } from "react";
+import { HiChevronDoubleLeft } from "react-icons/hi2";
+import { MdHome } from "react-icons/md";
+import { FaSearchengin } from "react-icons/fa";
+import { motion } from "framer-motion";
+import { Link, Route, Routes } from "react-router-dom";
+import { Logo } from "../assets";
+import { Projects, SignUp } from "../containers";
 
 const Home = () => {
-  
-  const [ isSideMenu, setIsSideMenu ]  = useState(false);
-  const [ user, setUser ] = useState(null);
+  const [isSideMenu, setIsSideMenu] = useState(false);
+  const [user, setUser] = useState(null);
 
   return (
     <>
-    <div 
-    style={{ backgroundColor: "#1E1F26"}}
-    className={`w-2 
-    ${
-      isSideMenu ? "w-2" : "flex-[.2] xl:flex-[.2]"
-    } 
+      <div
+        style={{ backgroundColor: "#1E1F26" }}
+        className={`w-2 
+    ${isSideMenu ? "w-2" : "flex-[.2] xl:flex-[.2]"} 
 
     min-h-screen max-h-screen relative px-3 py-6 flex
     flex-col items-center justify-start gap-4 transition-all
     duration-200 ease-in-out`}
-    >
-      {/* anchor */}
-      <motion.div 
-      whileTap={{ scale: 0.9 }}
-      onClick={() => setIsSideMenu(!isSideMenu)} 
-      style={{ backgroundColor: "#1E1F26"}}
-      className='w-8 h-8 rounded-tr-lg rounded-br-lg
-      absolute -right-6 flex items-center justify-center cursor-pointer'>
-        <HiChevronDoubleLeft className='text-white text-xl' />
-      </motion.div>
+      >
+        {/* anchor */}
+        <motion.div
+          whileTap={{ scale: 0.9 }}
+          onClick={() => setIsSideMenu(!isSideMenu)}
+          style={{ backgroundColor: "#1E1F26" }}
+          className="w-8 h-8 rounded-tr-lg rounded-br-lg
+      absolute -right-6 flex items-center justify-center cursor-pointer"
+        >
+          <HiChevronDoubleLeft className="text-white text-xl" />
+        </motion.div>
 
-      <div className='overflow-hidden w-full flex flex-col gap-4'>
-        
-      {/* logo */}
-      <Link to={"/home"}>
-        <img src={Logo} alt='logo' className='object-contain w-72 h-auto'/>
-      </Link>
+        <div className="overflow-hidden w-full flex flex-col gap-4">
+          {/* logo */}
+          <Link to={"/home"}>
+            <img src={Logo} alt="logo" className="object-contain w-72 h-auto" />
+          </Link>
 
-      {/* start coding */}
-      <Link to={"/newProject "}
-      className='px-6 py-3 rounded-xl flex items-center justify-center border border-gray-400 cursor-pointer
-      group hover:border-gray-200'>
-        <p className='text-gray-400 group:text-gray-200 capitalize'>
-          Start Coding
-        </p>
-      </Link>
+          {/* start coding */}
+          <Link
+            to={"/newProject "}
+            className="px-6 py-3 rounded-xl flex items-center justify-center border border-gray-400 cursor-pointer
+      group hover:border-gray-200"
+          >
+            <p className="text-gray-400 group:text-gray-200 capitalize">
+              Start Coding
+            </p>
+          </Link>
 
-      {/* home nav */}
-      {user && (
-        <Link to={"/home/projects"} 
-        className='flex items-center justify-center gap-4'>
-          <MdHome className='text-gray-400 text-xl' />
-          <p className='text-lg text-gray-400'>Home</p>
-        </Link>
-      )}
-
+          {/* home nav */}
+          {user && (
+            <Link
+              to={"/home/projects"}
+              className="flex items-center justify-center gap-4"
+            >
+              <MdHome className="text-gray-400 text-xl" />
+              <p className="text-lg text-gray-400">Home</p>
+            </Link>
+          )}
+        </div>
       </div>
-    </div>
-    
-    {/* Right side section */}
-    <div className='flex-1 min-h-screen max-h-screen overflow-y-scroll
+
+      {/* Right side section */}
+      <div
+        className="flex-1 min-h-screen max-h-screen overflow-y-scroll
     h-full flex flex-col items-start justify-start px-4 md:px-12 py-4
-    md:py-12'>
-      {/* Top section */}
-      <div className='w-full flex items-center justify-between gap-3'>
-        {/* Search */}
-        <div 
-        style={{ backgroundColor: "#1E1F26"}}
-        className='w-full px-4 py-3 rounded-md flex items-center justify-center gap-3'>
+    md:py-12"
+      >
+        {/* Top section */}
+        <div className="w-full flex items-center justify-between gap-3">
+          {/* Search */}
+          <div
+            style={{ backgroundColor: "#1E1F26" }}
+            className="w-full px-4 py-3 rounded-md flex items-center justify-center gap-3"
+          >
+            <FaSearchengin className="text-2xl text-gray-400" />
+            <input
+              type="text"
+              className="flex-1 px-4 py-1 text-xl bg-transparent
+          outline-none border-none text-gray-400 placeholder:text-gray-600"
+              placeholder="Search Here..."
+            />
+          </div>
 
-          <FaSearchengin className='text-2xl text-gray-400' />
-          <input type='text' 
-          className='flex-1 px-4 py-1 text-xl bg-transparent
-          outline-none border-none text-gray-400 placeholder:text-gray-600'
-          placeholder='Search Here...' />
+          {/* profile section */}
+          {!user && (
+            <motion.div
+              whileTap={{ scale: 0.9 }}
+              className="flex items-center justify-center gap-3"
+            >
+              <Link
+                to={"/home/auth"}
+                className="bg-emerald-500 px-6 py-2 rounded-md text-white text-lg cursor-pointer hover:bg-emerald-700"
+              >
+                SignUp
+              </Link>
+            </motion.div>
+          )}
 
+          {user && <div></div>}
         </div>
 
-        {/* profile section */}
-        {!user && (
-          <motion.div 
-          whileTap={{ scale: 0.9 }} 
-          className='flex items-center justify-center gap-3'>
-            <Link to={"/home/auth"} 
-            className='bg-emerald-500 px-6 py-2 rounded-md text-white text-lg cursor-pointer hover:bg-emerald-700'
-            >
-              SignUp
-            </Link>
-          </motion.div>
-        )}
-
-        { user && (
-          <div></div>
-        )}
-
+        {/* bottom section */}
+        <div className="w-full">
+          <Routes>
+            <Route path="/*" element={<Projects />} />
+            <Route path="/auth" element={<SignUp />} />
+          </Routes>
+        </div>
       </div>
-
-      {/* bottom section */}
-      <div className='w-full'>
-        <Routes>
-          <Route path="/*" element={<Projects />} />
-          <Route path="/auth" element={<SignUp />} />
-        </Routes>
-      </div>
-    </div>
     </>
-  )
-}
+  );
+};
 
 export default Home;
